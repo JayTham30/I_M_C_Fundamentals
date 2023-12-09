4f7800118e667b0e7c0f6cc8679c0fceee5a3870
Jay Thammavongsa <thammavongsajay@gmail.com>
2023-12-09 00:59:13 -0500

    Practice Task

--- hello.py
+++ hello.py
@@ -1,1 +1,26 @@
-print("Hello, World!")
+""" print("Hello, World!") """ #Task 1 
+
+
+"""name = input("Hello, may I ask what is your name? ")
+
+print(f"Hello {name.capitalize()}, it is nice to meet you!")"""  #Task 2
+
+
+"""first_number = input("What would you like your first number to be? ")
+
+second_number = input("What about your second number? ")
+
+sum = int(first_number) + int(second_number)
+difference = int(first_number) - int(second_number)
+product = int(first_number) * int(second_number)
+quotient = int(first_number) // int(second_number) 
+
+print(f"Sum: {sum} Difference: {difference} Product: {product} Quotient: {float(quotient)}") """ #Task 3
+
+
+age = input("How old are you? ")
+
+next_year = int(age) + 1
+
+print(f"Next year, you will be {next_year} years old")
+
